9a592a0da519004e985698be13ef99aadfcb23ba
fi-res <nta16022013@gmail.com>
2026-05-23 18:44:57 +0300

    feat(home,post): add tabs; add post stats and actions (not working except like and repost); fix images dimensions

--- app/screens/home.py
+++ app/screens/home.py
@@ -2,32 +2,61 @@ from asyncio import new_event_loop
 
 from textual import work
 from textual.app import ComposeResult
-from textual.widgets import LoadingIndicator
+from textual.widgets import LoadingIndicator, TabbedContent, TabPane
 from textual.containers import VerticalScroll
 from itd import Posts
+from itd.enums import PostsTab
 
 from app.screens.base import BaseScreen
 from app.widgets import PostWidget
 
 class HomeScreen(BaseScreen):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.posts_map = {
+            PostsTab.POPULAR: Posts(),
+            PostsTab.FOLLOWING: Posts.following(),
+            PostsTab.CLAN: Posts.clan()
+        }
+        self.tab = PostsTab.POPULAR
+
     def compose(self) -> ComposeResult:
         for widget in super().compose():
             yield widget
 
-        yield VerticalScroll(id='posts')
+        with TabbedContent():
+            with TabPane('Популярное', id='popular'):
+                yield VerticalScroll(id='popular-posts')
+
+            with TabPane('Подписки', id='following'):
+                yield VerticalScroll(id='following-posts')
+
+            with TabPane('Клан', id='clan'):
+                yield VerticalScroll(id='clan-posts')
+
 
     @work(exclusive=True)
     async def load_posts(self):
-        posts = self.query_one('#posts', VerticalScroll)
+        posts = self.query_one(f'#{self.tab.value}-posts', VerticalScroll)
         loading = LoadingIndicator()
         await posts.mount(loading)
 
-        for i, post in enumerate(Posts()):
+        for i, post in enumerate(self.posts):
             if i > 20:
                 break
             await posts.mount(PostWidget(post), before=loading)
 
         await loading.remove()
 
-    def on_mount(self):
-        self.load_posts()
+    # def on_mount(self):
+    #     self.load_posts()
+
+    def on_tabbed_content_tab_activated(self, event: TabbedContent.TabActivated):
+        self.tab = PostsTab((event.tab.id or '').replace('--content-tab-', ''))
+        if not self.posts:
+            self.load_posts()
+
+    @property
+    def posts(self):
+        return self.posts_map[self.tab]
--- app/widgets/post.py
+++ app/widgets/post.py
@@ -1,13 +1,28 @@
 from io import BytesIO
 
 from textual.app import ComposeResult
+from textual.message import Message
 from textual.widget import Widget
 from textual.widgets import Static
-from textual.containers import HorizontalScroll
-from textual_image.widget import SixelImage, SixelOptions
+from textual.containers import HorizontalScroll, Horizontal, Vertical
+from textual_image.widget import HalfcellImage
 from itd import Post
 from itd.enums import AttachType
 from requests import get
+
+
+class _ClickableStatic(Static):
+    class Clicked(Message):
+        def __init__(self, id: str) -> None:
+            super().__init__()
+            self.id = id
+
+    def __init__(self, content: str = "", *, id: str | None = None, classes: str | None = None) -> None:
+        super().__init__(content, id=id, classes=classes)
+
+    def on_click(self):
+        if self.id:
+            self.post_message(self.Clicked(self.id))
 
 
 class PostWidget(Widget):
@@ -16,8 +31,48 @@ class PostWidget(Widget):
         self.post = post
 
     def compose(self) -> ComposeResult:
+        with Horizontal(classes='post-author'):
+            yield Static(self.post.author.avatar, classes='post-author-avatar')
+            with Vertical(classes='post-author-name'):
+                yield Static(self.post.author.display_name)
+                yield Static(f'@{self.post.author.username}', classes='post-author-username')
+            with Horizontal(classes='post-actions'):
+                yield Static('󰒗', classes='post-share')
+                yield Static('', classes='post-copy')
+                yield Static('', classes='post-report')
+
         yield Static(self.post.content)
+
         if self.post.attachments:
-            yield HorizontalScroll(
-                *(SixelImage(BytesIO(get(attachment.url).content), classes='post-image', sixel_options=SixelOptions(64, False, 'maxcoverage')) for attachment in self.post.attachments if attachment.type == AttachType.IMAGE)
-            )
+            with HorizontalScroll(classes='post-images'):
+                for attachment in self.post.attachments:
+                    if attachment.type != AttachType.IMAGE:
+                        continue
+
+                    yield HalfcellImage(BytesIO(get(attachment.url).content), classes='post-image')
+
+        with Horizontal(classes='post-stats'):
+            yield _ClickableStatic(f'{"" if self.post.is_liked else ""} {self.post.likes_count}', classes=f'post-likes{" active" if self.post.is_liked else ""}', id='like')
+            yield _ClickableStatic(f' {self.post.comments_count}', classes='post-comments', id='comment')
+            yield _ClickableStatic(f'󰑖 {self.post.reposts_count}', classes=f'post-reposts{" active" if self.post.is_liked else ""}', id='repost')
+            yield _ClickableStatic(f' {self.post.views_count}', classes=f'post-views{" active" if self.post.is_viewed else ""}', id='view')
+
+
+    def on__clickable_static_clicked(self, event: _ClickableStatic.Clicked):
+        if event.id == 'like':
+            button = self.query_one('.post-likes', _ClickableStatic)
+
+            if self.post.is_liked:
+                button.remove_class('active')
+                self.post.unlike()
+                button.update(f' {self.post.likes_count}')
+            else:
+                button.add_class('active')
+                self.post.like()
+                button.update(f' {self.post.likes_count}')
+
+        if event.id == 'repost' and not self.post.is_reposted:
+            button = self.query_one('.post-reposts', _ClickableStatic)
+            button.add_class('active')
+            self.post.repost()
+            button.update(f'󰑖 {self.post.reposts_count}')
